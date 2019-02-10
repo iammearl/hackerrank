949c4f49528d6eae86d0ab001085f1d1c06eb9c2
Mearl <michael@michaelearl.me>
2019-02-10 12:18:59 -0700

    Mini Max Sum

--- nodejs/miniMaxSum/main.js
+++ nodejs/miniMaxSum/main.js
@@ -1,11 +1,15 @@
-// not done yet
-function miniMaxSum(arr) {
-	let out = [],
-		len = arr.length;
-
-	for (var i; i < len; i++) {
-		arr[i];
+function miniMaxSum(arr = [1,2,3,4,5]) {
+	let sumArr = Array(arr.length).fill(0),
+		i = arr.length-1;
+	while (i >= 0) {
+		let j = arr.length - 1
+		while (j >= 0) {
+			if (j !== i) sumArr[i] += arr[j];
+			j--;
+		}
+		i--;
 	}
-
-	return out;
+	sumArr.sort();
+	// return [sumArr[0], sumArr[arr.length-1]];
+	console.log(sumArr[0], sumArr[arr.length-1]);
 }
